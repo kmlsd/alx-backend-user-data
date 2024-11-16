bd5e1ef221295d1c51bdf54c38f5f178edc8dc71
kmlsd <kmlsd4757@gmail.com>
2024-11-16 13:05:26 -0800

    session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,65 +1,45 @@
 #!/usr/bin/env python3
-""" Module of Session Authentication
 """
+Session Authentication
+"""
+from typing import Dict
+from flask.globals import session
 from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
 
 
 class SessionAuth(Auth):
-    """Session Authentication Class"""
-    user_id_by_session_id = {}
+    """ Session class inherits Auth """
+    user_id_by_session_id: Dict[str, str] = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a Session ID for a user_id"""
-
+        """ Session ID Generator """
         if user_id is None or not isinstance(user_id, str):
             return None
-
         session_id = str(uuid.uuid4())
-
         self.user_id_by_session_id[session_id] = user_id
-
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a User ID based on a Session ID"""
-
+        """ Returns session id based on user id """
         if session_id is None or not isinstance(session_id, str):
             return None
-
-        return self.user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id, None)
 
     def current_user(self, request=None):
-        """Returns a User instance based on a cookie value"""
-
-        session_id = self.session_cookie(request)
-
-        if session_id is None:
-            return None
-
-        user_id = self.user_id_for_session_id(session_id)
-
-        return User.get(user_id)
+        """ Returns User based cookie value """
+        cookie = self.session_cookie(request)
+        session_user_id = self.user_id_for_session_id(cookie)
+        user_id = User.get(session_user_id)
+        return user_id
 
     def destroy_session(self, request=None):
-        """Deletes de user session / logout"""
-
-        if request is None:
+        """ Deletes user session / login(out) """
+        cookie_data = self.session_cookie(request)
+        if cookie_data is None:
             return False
-
-        session_id = self.session_cookie(request)
-        if session_id is None:
+        if not self.user_id_for_session_id(cookie_data):
             return False
-
-        user_id = self.user_id_for_session_id(session_id)
-
-        if not user_id:
-            return False
-
-        try:
-            del self.user_id_by_session_id[session_id]
-        except Exception:
-            pass
-
+        del self.user_id_by_session_id[cookie_data]
         return True
